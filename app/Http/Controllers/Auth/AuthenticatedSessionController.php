389e1949135a27e383d40588fd16fe9db1af4440
MariKato <marikato923@gmail.com>
2025-02-11 20:50:45 +0900

    認証成功時の画面遷移について修正

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->flash('success', 'ログインしました。');
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->route('home');
     }
 
     /**
@@ -45,7 +45,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-
+        $request->session()->flash('success', 'ログアウトしました。');
 
         return redirect('/login');
     }
