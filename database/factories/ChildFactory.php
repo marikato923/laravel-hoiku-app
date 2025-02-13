e41b70f81e0ced3049d97986fb0c31283ab76fa5
MariKato <marikato923@gmail.com>
2025-02-14 01:20:16 +0900

    ChildFactoryを作成

--- database/factories/ChildFactory.php
+++ database/factories/ChildFactory.php
@@ -2,40 +2,32 @@
 
 namespace Database\Factories;
 
-use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Child;
+use App\Models\User;
+use App\Models\Classroom;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Child>
- */
 class ChildFactory extends Factory
 {
     protected $model = Child::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
-        $faker = \Faker\Factory::create('ja_JP');
-
-        $lastNameKana = mb_convert_kana($faker->lastName(), 'K');
-        $firstNameKana = mb_convert_kana($faker->firstName(), 'K');
-
         return [
-            'last_name' => $this->faker->lastName(),         
-            'first_name' => $this->faker->firstName(),        
-            'last_kana_name' => $lastNameKana,  
-            'first_kana_name' => $firstNameKana,
-            'birthdate' => $this->faker->date(),
-            'img' => '',
-            'admission_date' => $this->faker->date(),
-            'medical_history' => $this->faker->text(), 
-            'has_allergy' => $this->faker->boolean(), 
-            'allergy_type' => $this->faker->boolean() ? $this->faker->word() :null,
+            'last_name' => $this->faker->lastName,
+            'first_name' => $this->faker->firstName,
+            'last_kana_name' => 'タナカ', 
+            'first_kana_name' => 'タロウ',
+            'birthdate' => $this->faker->date('Y-m-d', '-5 years'), 
+            'admission_date' => $this->faker->date('Y-m-d', '-5 years'), 
+            'medical_history' => null,
+            'has_allergy' => false,
+            'allergy_type' => null,
+            'user_id' => null, 
+            'classroom_id' => null, 
+            'status' => 'approved',
+            'img' => null,
         ];
     }
 }
