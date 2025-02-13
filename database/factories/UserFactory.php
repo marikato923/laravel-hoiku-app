471bc33b6f5dc8dbfb7e66b52f31f88111c48c45
MariKato <marikato923@gmail.com>
2025-02-14 01:05:32 +0900

    ユーザーのダミーデータを作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,55 +2,30 @@
 
 namespace Database\Factories;
 
-use Faker\Generator as Faker;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
-        $faker = \Faker\Factory::create('ja_JP'); 
-
-        $lastNameKana = mb_convert_kana($faker->lastName(), 'K');
-        $firstNameKana = mb_convert_kana($faker->firstName(), 'K');
-
         return [
-            'last_name' => $this->faker->lastName(),         
-            'first_name' => $this->faker->firstName(),        
-            'last_kana_name' => $lastNameKana,  
-            'first_kana_name' => $firstNameKana,
-            'email' => fake()->unique()->safeEmail(),
+            'last_name' => $this->faker->lastName,
+            'first_name' => $this->faker->firstName,
+            'last_kana_name' => 'タナカ', 
+            'first_kana_name' => 'タロウ',
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password123'), 
+            'phone_number' => $this->faker->numerify('080#######'),
+            'postal_code' => $this->faker->numerify('1234567'), 
+            'address' => $this->faker->address,
+            'push_subscription' => json_encode([]), 
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'phone_number' => '00000000000',
-            'postal_code' => '0000000',
-            'address' => 'テスト',
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
