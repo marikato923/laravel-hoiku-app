dd18d1325f7cb6673b5666773ed328efc9279d53
MariKato <marikato923@gmail.com>
2025-02-14 01:54:47 +0900

    AttendanceFactoryを作成

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -2,27 +2,22 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Attendance;
 use App\Models\Child;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Attendance>
- */
 class AttendanceFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Attendance::class;
+
+    public function definition()
     {
         return [
-            'child_id' => Child::inRandomOrder()->first()->id,
-            'arrival_time' => $this->faker->time('H:i', '08:00'),
-            'departure_time' => $this->faker->time('H:i', '17:00'),
-            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
-            'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'child_id' => null, 
+            'arrival_time' => null, 
+            'departure_time' => null, 
+            'pickup_time' => null,
         ];
     }
 }
