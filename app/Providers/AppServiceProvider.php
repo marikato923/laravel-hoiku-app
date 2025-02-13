c38274243a2149005fd6644e1108e0de09c35268
MariKato <marikato923@gmail.com>
2025-02-13 11:45:52 +0900

    環境設定を編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (App::environment(['production'])) {
-            URL::forceScheme('https');
+           // URL::forceScheme('https');
         }
 
         Paginator::useBootstrap();
